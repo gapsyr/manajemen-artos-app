9b5ef515fbd851c8e943e636fcb123846bf13498
gapsyr <iculgapsyr@gmail.com>
2026-06-04 13:01:22 +0700

    Tambah validasi input transaksi

--- src/controllers/transaksiController.js
+++ src/controllers/transaksiController.js
@@ -1,11 +1,47 @@
-const transaksi = [];
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "../../data/transaksi.json");
+
+function bacaData() {
+  const data = fs.readFileSync(filePath, "utf8");
+  return JSON.parse(data);
+}
+
+function simpanData(data) {
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(data, null, 2)
+  );
+}
 
 exports.getTransaksi = (req, res) => {
+  const transaksi = bacaData();
   res.json(transaksi);
 };
 
 exports.tambahTransaksi = (req, res) => {
+  const transaksi = bacaData();
+
   const { kategori, nominal, tipe } = req.body;
+
+if (!kategori || kategori.trim() === "") {
+  return res.status(400).json({
+    error: "Kategori wajib diisi"
+  });
+}
+
+if (isNaN(nominal) || Number(nominal) <= 0) {
+  return res.status(400).json({
+    error: "Nominal harus berupa angka lebih dari 0"
+  });
+}
+
+if (!["masuk", "keluar"].includes(tipe)) {
+  return res.status(400).json({
+    error: "Tipe hanya boleh masuk atau keluar"
+  });
+}
 
   const dataBaru = {
     id: Date.now(),
@@ -17,6 +53,8 @@ exports.tambahTransaksi = (req, res) => {
 
   transaksi.push(dataBaru);
 
+  simpanData(transaksi);
+
   res.status(201).json({
     message: "Transaksi berhasil ditambahkan",
     data: dataBaru
@@ -24,6 +62,8 @@ exports.tambahTransaksi = (req, res) => {
 };
 
 exports.getSaldo = (req, res) => {
+  const transaksi = bacaData();
+
   let saldo = 0;
 
   transaksi.forEach(item => {
